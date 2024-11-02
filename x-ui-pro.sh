c3420f298229a94e3b11a939f52e2915a6fca035
Ivan Razin <mozaroc@gmail.com>
2024-11-02 23:10:24 +0200

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -532,7 +532,8 @@ if [[ -f $XUIDB ]]; then
 	     '',
              '8443',
 	     'vless',
-             '{"clients": [
+             '{
+	     "clients": [
     {
       "id": "${client_id}",
       "flow": "xtls-rprx-vision",
