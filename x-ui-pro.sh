a7bc0d852ae96c9ddda5eddb432753f7220c5878
Ivan Razin <mozaroc@gmail.com>
2024-11-02 22:08:09 +0200

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -473,7 +473,8 @@ var1=$(/usr/local/x-ui/bin/xray-linux-amd64 x25519)
 var2=($var1)
 private_key=${var2[2]}
 public_key= ${var2[5]}
-
+echo ${private_key}
+echo ${public_key}
 ########################################Update X-UI Port/Path for first INSTALL#########################
 UPDATE_XUIDB(){
 if [[ -f $XUIDB ]]; then
